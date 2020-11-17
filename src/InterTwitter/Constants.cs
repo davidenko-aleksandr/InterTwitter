3ee40e7b62f3a648ed40953008680e8cd87f72a3
Sassha5 <udovikasasha@gmail.com>
2020-11-17 18:26:14 +0200

    Configured projects

---
+++ src/InterTwitter/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace InterTwitter
+{
+    public class Constants
+    {
+#if RELEASE
+        public const string BASE_URL = "";
+#elif STAGE
+        public const string BASE_URL = "";
+#elif DEV
+        public const string BASE_URL = "";
+#else
+        public const string BASE_URL = "";
+#endif
+    }
+}
