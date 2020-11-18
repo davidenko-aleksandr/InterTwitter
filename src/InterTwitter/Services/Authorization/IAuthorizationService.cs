aae48f0c3256a2f6f03e443b005e157be6495f69
Sassha5 <udovikasasha@gmail.com>
2020-11-18 16:20:35 +0200

    - Added user model - Added AOResult helper - Added Authorization service with mock

---
+++ src/InterTwitter/Services/Authorization/IAuthorizationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using InterTwitter.Helpers;
+
+namespace InterTwitter.Services.Authorization
+{
+    public interface IAuthorizationService
+    {
+        Task<AOResult<bool>> LogInAsync(string email, string password);
+        Task<AOResult<bool>> SignUpAsync(string email, string name, string password);
+    }
+}
