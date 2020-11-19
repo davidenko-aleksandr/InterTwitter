a0a1bf5a9cb40414d8f93e64cefb048a69acb73c
alexandr_davidenko <alex1190119022@gmail.com>
2020-11-19 12:10:05 +0200

    - Add a custon navigation bar - Add a ClickableContentView

---
+++ src/InterTwitter/Controls/CustomSignInNavigationBar.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace InterTwitter.Controls
+{
+    public partial class CustomSignInNavigationBar : ContentView
+    {
+        #region -- Public properties --
+
+        public CustomSignInNavigationBar()
+        {
+            InitializeComponent();
+        }
+
+        public static BindableProperty IsShowBackButtonProperty = BindableProperty.Create(
+                                                                                        propertyName: nameof(IsShowBackButton),
+                                                                                        returnType: typeof(bool),
+                                                                                        declaringType: typeof(CustomSignInNavigationBar),
+                                                                                        defaultValue: false);
+        public bool IsShowBackButton
+        {
+            get => (bool)GetValue(IsShowBackButtonProperty);
+            set => SetValue(IsShowBackButtonProperty, value);
+        }
+
+        public static BindableProperty GoBackCommandProperty = BindableProperty.Create(
+                                                                                        propertyName: nameof(GoBackCommand),
+                                                                                        returnType: typeof(ICommand),
+                                                                                        declaringType: typeof(CustomSignInNavigationBar),
+                                                                                        defaultBindingMode: BindingMode.TwoWay,
+                                                                                        defaultValue: null);
+        public ICommand GoBackCommand
+        {
+            get => (ICommand)GetValue(GoBackCommandProperty);
+            set => SetValue(GoBackCommandProperty, value);
+        }
+
+        #endregion
+    }
+}
