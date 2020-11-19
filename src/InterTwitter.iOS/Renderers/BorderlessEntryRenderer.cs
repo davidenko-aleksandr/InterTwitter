39c48e398097a52826d97de2cedd45818640c188
Alex <47815470+TheB1ack@users.noreply.github.com>
2020-11-19 12:47:27 +0200

    - Added custom Entry

---
+++ src/InterTwitter.iOS/Renderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,34 @@
+﻿using InterTwitter.Controls;
+using InterTwitter.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace InterTwitter.iOS.Renderers
+{
+    public class BorderlessEntryRenderer : EntryRenderer
+    {
+
+        #region -- Overrides --
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            
+            if (Control != null)
+            {
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Control is null");
+            }
+
+        }
+
+        #endregion
+
+    }
+}
