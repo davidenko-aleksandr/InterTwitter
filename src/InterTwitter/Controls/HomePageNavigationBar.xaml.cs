d3f94cb4812d700f0d285d5f3fa8e5e3de3be727
alexandr_davidenko <alex1190119022@gmail.com>
2020-11-19 21:19:18 +0200

    - Add a HomeNavigationBar

---
+++ src/InterTwitter/Controls/HomePageNavigationBar.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace InterTwitter.Controls
+{
+    public partial class HomePageNavigationBar : Grid
+    {
+        public HomePageNavigationBar()
+        {
+            InitializeComponent();
+        }
+
+        #region -- Public properties --
+
+        public static BindableProperty GoBackCommandProperty = BindableProperty.Create(
+                                                                                        propertyName: nameof(GoBackCommand),
+                                                                                        returnType: typeof(ICommand),
+                                                                                        declaringType: typeof(AuthorizationNavigationBar));
+        public ICommand GoBackCommand
+        {
+            get => (ICommand)GetValue(GoBackCommandProperty);
+            set => SetValue(GoBackCommandProperty, value);
+        }
+
+        public static BindableProperty EditCommandProperty = BindableProperty.Create(
+                                                                                        propertyName: nameof(EditCommand),
+                                                                                        returnType: typeof(ICommand),
+                                                                                        declaringType: typeof(AuthorizationNavigationBar));
+        public ICommand EditCommand
+        {
+            get => (ICommand)GetValue(EditCommandProperty);
+            set => SetValue(EditCommandProperty, value);
+        }
+
+        #endregion
+    }
+}
