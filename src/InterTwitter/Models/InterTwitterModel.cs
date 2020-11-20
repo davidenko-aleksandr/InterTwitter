a975006448da1f745b37aad16b99375cf91c3061
alexandr_davidenko <alex1190119022@gmail.com>
2020-11-20 18:18:19 +0200

    - Home_Page -Add Model

---
+++ src/InterTwitter/Models/InterTwitterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InterTwitter.Models
+{
+    public class InterTwitterModel
+    {
+        public string AuthorAvatar { get; set; }
+
+        public string PostLabel { get; set; }
+
+        public string PostDescription { get; set; }
+
+        public DateTime PostDateTime { get; set; }
+
+        public string PostPhoto { get; set; }
+    }
+}
