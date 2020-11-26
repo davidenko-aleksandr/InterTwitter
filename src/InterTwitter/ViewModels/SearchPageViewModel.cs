12ea840151d1a625c00ab37b94e4e4cd343da4d5
Elijah <mr.andreev2012@gmail.com>
2020-11-26 17:11:26 +0200

    -base SearchPage ui added

--- src/InterTwitter/ViewModels/SearchPageViewModel.cs
+++ src/InterTwitter/ViewModels/SearchPageViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+using InterTwitter.Models;
+using InterTwitter.Services.Authorization;
 using Prism.Navigation;
 
 namespace InterTwitter.ViewModels
 {
     public class SearchPageViewModel : BaseViewModel
     {
-        public SearchPageViewModel(INavigationService navigationService)
+        private readonly IAuthorizationService _authorizationService;
+        public SearchPageViewModel(
+            INavigationService navigationService,
+            IAuthorizationService authorizationService)
                                   : base(navigationService)
         {
+            _authorizationService = authorizationService;
         }
 
         #region -- Public Properties --
@@ -19,18 +26,37 @@ namespace InterTwitter.ViewModels
             set => SetProperty(ref _icon, value);
         }
 
+        private UserModel _authorizedUser;
+        public UserModel AuthorizedUser
+        {
+            get => _authorizedUser;
+            set => SetProperty(ref _authorizedUser, value);
+        }
+
         #endregion
 
         #region -- Overrides --
 
-        public override void OnNavigatedTo(INavigationParameters parameters)
+        public override async void OnNavigatedTo(INavigationParameters parameters)
         {
             Icon = "ic_search_blue";
+
+            await SetUserDataAsync();
         }
 
         public override void OnNavigatedFrom(INavigationParameters parameters)
         {
             Icon = "ic_search_gray";
+        }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private async Task SetUserDataAsync()
+        {
+            var result = await _authorizationService.GetAuthorizedUserAsync();
+            AuthorizedUser = result.Result;
         }
 
         #endregion
